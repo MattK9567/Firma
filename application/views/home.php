d688dd8bdfdfcc16dc3f7fd6bb0a99af65b1ddaf
MattK9567 <codename4756@gmail.com>
2017-05-31 18:07:18 +0200

    Pridany jeden chart - funguje

--- application/views/home.php
+++ application/views/home.php
@@ -1,47 +1,47 @@
-<html>
-<head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
+<div class="content-wrapper">
+    <h2>das</h2>
+    <table class="columns">
+        <tr>
+            <td><?php
+                $dbhandle = new mysqli('localhost','root','','sportoviska');
+                $query = "SELECT sportoviska.nazov as title, rezervacia.sportoviska_ID, COUNT(rezervacia.ID) as number
+                          FROM rezervacia 
+                          JOIN sportoviska
+                          ON sportoviska.ID = rezervacia.sportoviska_ID
+                          GROUP BY sportoviska.nazov";
+                $res = $dbhandle->query($query);
+                ?>
+                <section class="content-header">
 
-        // Load the Visualization API and the corechart package.
-        google.charts.load('current', {'packages':['corechart']});
-
-        // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(drawChart);
-
-        // Callback that creates and populates a data table,
-        // instantiates the pie chart, passes in the data and
-        // draws it.
-        function drawChart() {
-
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-                ['Mushrooms', 3],
-                ['Onions', 1],
-                ['Olives', 1],
-                ['Zucchini', 1],
-                ['Pepperoni', 2]
-            ]);
-
-            // Set chart options
-            var options = {'title':'How Much Pizza I Ate Last Night',
-                'width':400,
-                'height':300};
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
-    </script>
-</head>
-
-<body>
-<h2>das</h2>
-<!--Div that will hold the pie chart-->
-<div id="chart_div"></div>
-</body>
-</html>
+                    <html>
+                    <head>
+                        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                        <script type="text/javascript">
+                            google.charts.load("current", {packages:["piechart"]});
+                            google.charts.setOnLoadCallback(drawChart);
+                            function drawChart() {
+                                var data = google.visualization.arrayToDataTable([
+                                    ['title', 'number'],
+                                    <?php
+                                    while ($row= $res->fetch_assoc())                        {
+                                        echo "['".$row['title']."',".$row['number']."],";
+                                    }
+                                    ?>
+                                ]);
+                                var options = {
+                                    title: 'Count of lectors per agency',
+                                    pieHole: 0.4,
+                                };
+                                var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+                                chart.draw(data, options);
+                            }
+                        </script>
+                    </head>
+                    <body>
+                        <div class="border-right" id="donutchart" style="width: 800px; height: 400px;"></div>
+                    </body>
+                    </html>
+            </td>
+        </tr>
+    </table>
+</div>
